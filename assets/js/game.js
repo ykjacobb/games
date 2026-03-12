cb8e8aff2d50c5d17e7f3282b7540a5538ce0b65
Jacob <jacoblpbiz@gmail.com>
2026-03-12 17:09:36 +0000

    new games

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,10 +3,15 @@ window.addEventListener("load", (event) => {
     const gameName= gameFrame?.name ?? "";
     if (gameName!= "") {
         console.log("LoadGame", gameName);
-        if (gameName === "PaperIO2") {
-            gameFrame.src= "games/paperio2/index.html";
+        const localGames = {
+            "PaperIO2": "games/paperio2/index.html",
+            "Cubes2048IO": "games/cubes2048/index.html",
+            "NitroKnightsIO": "games/nitroknights/index.html"
+        };
+        if (localGames[gameName]) {
+            gameFrame.src = localGames[gameName];
         } else {
-            gameFrame.src= `https://ubg17.github.io/${gameName}/`;
+            gameFrame.src = `https://ubg17.github.io/${gameName}/`;
         }
     }    
 });
