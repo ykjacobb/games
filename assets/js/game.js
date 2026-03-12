545909e7f75acea7f430562502a4b9237cd1d68b
Jacob <jacoblpbiz@gmail.com>
2026-03-12 16:59:17 +0000

    assets

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,6 +3,10 @@ window.addEventListener("load", (event) => {
     const gameName= gameFrame?.name ?? "";
     if (gameName!= "") {
         console.log("LoadGame", gameName);
-        gameFrame.src= `https://ubg17.github.io/${gameName}/`;
+        if (gameName === "PaperIO2") {
+            gameFrame.src= "games/paperio2/index.html";
+        } else {
+            gameFrame.src= `https://ubg17.github.io/${gameName}/`;
+        }
     }    
 });
