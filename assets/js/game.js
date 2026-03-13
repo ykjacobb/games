87544e9e773dd5d0c54d8bf261fc3f031d7bf31e
Jacob <jacoblpbiz@gmail.com>
2026-03-13 07:16:56 +0000

    seperate ids so it cant get blocked

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,10 +4,10 @@ window.addEventListener("load", (event) => {
     if (gameName!= "") {
         console.log("LoadGame", gameName);
         const localGames = {
-            "PaperIO2": "games/paperio2/index.html",
-            "Cubes2048IO": "games/cubes2048/index.html",
-            "NitroKnightsIO": "games/nitroknights/index.html",
-            "BuckshotRoulette": "games/buckshot-roulette/index.html"
+            "PaperIO2": "games/defc920c-9942-472d-8c2f-cb527e0ff766/index.html",
+            "Cubes2048IO": "games/52355670-876d-4285-9fb4-16fda40d1a2f/index.html",
+            "NitroKnightsIO": "games/ea653047-57cf-4c68-b581-203b26dfe5cc/index.html",
+            "BuckshotRoulette": "games/0ee60948-40d6-4306-a8d9-329a3297d658/index.html"
         };
         if (localGames[gameName]) {
             gameFrame.src = localGames[gameName];
